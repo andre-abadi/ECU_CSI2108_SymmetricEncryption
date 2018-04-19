beb663be31d34fc7b1bb66e25e39d5c99a0a5419
dancingborg <dancingborg@users.noreply.github.com>
2018-04-19 14:21:10 +0800

    start padding code

--- assignment2_v0.1.py
+++ assignment2_v0.1.py
@@ -1,40 +1,36 @@
-# CSI2108 Semester 1 2018
-# Assessable Workshop - Symmetric Encryption
+"""CSI2108 Semester 1 2018: Assessable Workshop - Symmetric Encryption."""
 # Main source for code:
 # https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/
 
-# Setup
+# Import libraries
 import os
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-backend = default_backend()
 
-#Initialise
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+# Object initialisation
 key = os.urandom(32)
 iv = os.urandom(16)
-
-# Creat a new instance of a Cipher object
+backend = default_backend()
 settings = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-
-# Create an instance of an encryptor object
 encryptor = settings.encryptor()
 decryptor = settings.decryptor()
 
 # Create a secret message
 message = "a secret message"
-# Print said message
 print(message)
-
 # Convert message from String abstraction to literal bytes
 message = message.encode()
-# Print the variable (it will print as a String prepended by 'b')
-print(message)
 
-#Actually do the encryption
+# https://cryptography.io/en/latest/hazmat/primitives/padding/
+padder = padding.PKCS7(256).padder()
+
+# Actually do the encryption
 ciphertext = encryptor.update(message) + encryptor.finalize()
 print(ciphertext)
 
-# Creat an instance of a decryptor object
+# Do the decryption
 plaintext = decryptor.update(ciphertext) + decryptor.finalize()
 plaintext = plaintext.decode()
 print(plaintext)
