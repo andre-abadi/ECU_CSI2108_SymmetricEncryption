7d136c5cd56f6d3ee88586bf859c61c742492d5e
dancingborg <dancingborg@users.noreply.github.com>
2018-04-19 13:44:46 +0800

    working encryption and decryption of hardcoded message. no padding yet

--- assignment2_v0.1.py
+++ assignment2_v0.1.py
@@ -2,7 +2,38 @@
 # Assessable Workshop - Symmetric Encryption
 
 # https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/
+# Setup
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 backend = default_backend()
+
+#Initialise
+key = os.urandom(32)
+iv = os.urandom(16)
+
+# Creat a new instance of a Cipher object
+settings = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+
+# Create an instance of an encryptor object
+encryptor = settings.encryptor()
+decryptor = settings.decryptor()
+
+# Create a secret message
+message = "a secret message"
+# Print said message
+print(message)
+
+# Convert message from String abstraction to literal bytes
+message = message.encode()
+# Print the variable (it will print as a String prepended by 'b')
+print(message)
+
+#Actually do the encryption
+ciphertext = encryptor.update(message) + encryptor.finalize()
+print(ciphertext)
+
+# Creat an instance of a decryptor object
+plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+plaintext = plaintext.decode()
+print(plaintext)
