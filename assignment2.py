410a9dbb602fed5adbecbbcc34174acbc0ebe314
dancingborg <dancingborg@users.noreply.github.com>
2018-04-20 19:23:44 +0800

    add some basic input flow control for empty inputs

--- assignment2.py
+++ assignment2.py
@@ -5,13 +5,35 @@
 
 # Import libraries
 import os
+# https://stackoverflow.com/questions/17958347/how-can-i-convert-a-python-urandom-to-a-string
+from base64 import b64encode
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
+
+print("CSI2108 Symmetric Encryption Tool")
+passphrase = input("Please enter passphrase: ")
+print("Your passphrase is: " + passphrase)
+# Convert passphrase from String to bytes data type
+filename = input("Please enter a filename to be encrypted: ")
+if len(filename) == 0:
+    filename = "input.txt"
+    print("No input provided, using: " + filename)
+print("The filename to be encrypted is: " + filename)
+
+file = open(filename, mode='r')
+contents = file.read()
+print("The file was read and contained: " + contents)
+
+
 # Object initialisation
 key = os.urandom(32)
+key_string = b64encode(key).decode()
+print("The randomly generated key is: " + key_string)
 iv = os.urandom(16)
+iv_string = b64encode(iv).decode()
+print("The randomly generated IV is:  " + iv_string)
 backend = default_backend()
 settings = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
 padder = padding.PKCS7(128).padder()
@@ -42,14 +64,3 @@ unpadded = unpadder.update(decrypted)
 unpadded += unpadder.finalize()
 plaintext = unpadded.decode()
 print(plaintext)
-
-print("CSI2108 Symmetric Encryption Tool")
-passphrase = input("Please enter passphrase: ")
-print("Your filename is: " + passphrase)
-# Convert passphrase from String to bytes data type
-filename = input("Please enter a filename to be encrypted: ")
-filename = "input.txt"
-print("The filename to be encrypted is: " + filename)
-
-file = open(filename, mode='r')
-file.write(passphrase)
