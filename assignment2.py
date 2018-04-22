03b2b4899b22a28ba74419026c572a0eedaa9f25
dancingborg <dancingborg@users.noreply.github.com>
2018-04-22 20:23:27 +0800

    begin passphrase error checking, this commit not working fully

--- assignment2.py
+++ assignment2.py
@@ -5,44 +5,50 @@
 
 # Import libraries
 import os
-# https://stackoverflow.com/questions/17958347/how-can-i-convert-a-python-urandom-to-a-string
+import sys
 from base64 import b64encode
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
-
+# Preamble
 print("CSI2108 Symmetric Encryption Tool\n")
+# Input passphrase
 passphrase = input("Please enter passphrase: ")
 if len(passphrase) == 0:
-    passphrase = "TestPassphrase2018"
-    print("No passphrase provided, using: " + passphrase)
+    passphrase = "CSI2108"
+    print("No passphrase detected, defaulting to: " + passphrase)
+while ((sys.getsizeof(passphrase) * 8) != 256):
+    passphrase = input(
+        "{} is not 256, try again: "
+        .format(sys.getsizeof(passphrase) * 8))
+print("Passphrase is {} bits long".format(sys.getsizeof(passphrase) * 8))
 # while len(passphrase) == 0:
 #     passphrase = input("No passphrase detected! Please enter passphrase: ")
 print("Your passphrase is: " + passphrase)
-# Convert passphrase from String to bytes data type
+# Input filename
 filename = input("Please enter a filename to be encrypted: ")
 if len(filename) == 0:
     filename = "input.txt"
-    print("No filename provided, using: " + filename)
+    print("No filename detecting, defaulting to: " + filename)
 print("The filename to be encrypted is: " + filename)
 file = open(filename, mode='r')
-contents = file.read()
-print("The file was read and contained: " + contents)
-
-
-print("\nBegin hardcoded encryption-decryption sequence:")
-# Object initialisation
-key = os.urandom(32)
-key_string = b64encode(key).decode()
-print("The randomly generated key is: " + key_string)
+input = file.read()
+print("The file was read and contained: " + input)
+# Convert message to bytes
+input = input.encode()
+print("The message as bytes is: " + b64encode(input).decode())
+# Pad message
+padder = padding.PKCS7(16).padder()
+padded = padder.update(input)
+padded += padder.finalize()
+print("The padded message is: " + b64encode(padded).decode())
+# Generate Initialisation Vector (IV)
 iv = os.urandom(16)
-iv_string = b64encode(iv).decode()
-print("The randomly generated IV is:  " + iv_string)
+print("The Initialisation Vector (IV) is:  " + b64encode(iv).decode())
 backend = default_backend()
-settings = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-padder = padding.PKCS7(128).padder()
-unpadder = padding.PKCS7(128).unpadder()
+settings = Cipher(algorithms.AES(passphrase), modes.CBC(iv), backend=backend)
+
 encryptor = settings.encryptor()
 decryptor = settings.decryptor()
 # Create a secret message
@@ -51,9 +57,7 @@ print(message)
 # Convert message from String abstraction to literal bytes
 message = message.encode()
 # Pad the messaage to 256 bits
-padded = padder.update(message)
-padded += padder.finalize()
-print(padded)
+
 # Do the encryption
 encrypted = encryptor.update(padded) + encryptor.finalize()
 print(encrypted)
@@ -61,6 +65,7 @@ print(encrypted)
 decrypted = decryptor.update(encrypted) + decryptor.finalize()
 print(decrypted)
 # Unpad the decrypted message
+unpadder = padding.PKCS7(128).unpadder()
 unpadded = unpadder.update(decrypted)
 unpadded += unpadder.finalize()
 plaintext = unpadded.decode()
