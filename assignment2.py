b976ca2f4c27c60c9ac456eeed91e9410e1289f0
dancingborg <dancingborg@users.noreply.github.com>
2018-04-22 21:54:56 +0800

    all major tasks converted to functions, leaving interactives to last

--- assignment2.py
+++ assignment2.py
@@ -5,16 +5,11 @@
 
 # Import libraries
 import os
-import sys
 import hashlib
 from base64 import b64encode
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-
-# Preamble
-print("CSI2108 Symmetric Encryption Tool\n")
-isPadded = False
 
 
 def _createKey(passphrase):
@@ -36,6 +31,35 @@ def _createMessage(filename):
     return message
 
 
+def _encrypt(message):
+    padder = padding.PKCS7(128).padder()
+    padded = padder.update(message)
+    padded += padder.finalize()
+    message = padded
+    backend = default_backend()
+    settings = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    encryptor = settings.encryptor()
+    encrypted = encryptor.update(message) + encryptor.finalize()
+    print(encrypted)
+    return encrypted
+
+
+def _decrypt(ciphertext):
+    backend = default_backend()
+    settings = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    decryptor = settings.decryptor()
+    decrypted = decryptor.update(ciphertext) + decryptor.finalize()
+    # Unpad the decrypted message
+    unpadder = padding.PKCS7(128).unpadder()
+    unpadded = unpadder.update(decrypted)
+    unpadded += unpadder.finalize()
+    plaintext = unpadded.decode()
+    print(plaintext)
+    return plaintext
+
+
+# Preamble
+print("CSI2108 Symmetric Encryption Tool\n")
 # Input passphrase
 passphrase = input("Please enter passphrase: ")
 key = _createKey(passphrase)
@@ -45,29 +69,5 @@ message = _createMessage(filename)
 # Generate Initialisation Vector (IV)
 iv = os.urandom(16)
 print("The Initialisation Vector (IV) is:  " + b64encode(iv).decode())
-
-# Encryption
-if (sys.getsizeof(message)*8) % 128 != 0:
-    padder = padding.PKCS7(128).padder()
-    padded = padder.update(message)
-    padded += padder.finalize()
-    message = padded
-    isPadded = True
-backend = default_backend()
-settings = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-encryptor = settings.encryptor()
-encrypted = encryptor.update(message) + encryptor.finalize()
-
-
-# Decryption
-backend = default_backend()
-settings = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-decryptor = settings.decryptor()
-decrypted = decryptor.update(encrypted) + decryptor.finalize()
-# Unpad the decrypted message
-if isPadded is True:
-    unpadder = padding.PKCS7(128).unpadder()
-    unpadded = unpadder.update(decrypted)
-    unpadded += unpadder.finalize()
-    plaintext = unpadded.decode()
-    print(plaintext)
+secret_message = _encrypt(message)
+_decrypt(secret_message)
