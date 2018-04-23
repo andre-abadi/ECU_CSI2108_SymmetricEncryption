b4af18d68d20e3f79961222c5e0064cb2512ebc7
dancingborg <dancingborg@users.noreply.github.com>
2018-04-23 20:55:00 +0800

    working file output function _writeFile

--- assignment2.py
+++ assignment2.py
@@ -1,7 +1,5 @@
 """CSI2108 Semester 1 2018: Assessable Workshop - Symmetric Encryption."""
-# Main source for code:
-# https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/
-# https://cryptography.io/en/latest/hazmat/primitives/padding/
+
 
 # Import libraries
 import os
@@ -12,22 +10,37 @@ from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
-def _createKey(passphrase):
-    if len(passphrase) == 0:
-        passphrase = "CSI2108"
-        print("No passphrase detected, defaulting to: " + passphrase)
-    key = hashlib.sha256(passphrase.encode()).digest()
+def _createKey(pwd: str):
+    if len(pwd) == 0:
+        pwd = "CSI2108"
+        print("No passphrase detected, defaulting to: " + pwd)
+    key = hashlib.sha256(pwd.encode()).digest()
     return key
 
 
-def _createMessage(filename):
-    if len(filename) == 0:
-        filename = "input.txt"
-        print("No filename detected, defaulting to: " + filename)
-    file = open(filename, mode='r')
-    message = file.read()
-    message = message.encode()
-    return message
+def _readFile(fnm: str):
+    if len(fnm) == 0:
+        fnm = "input.txt"
+        print("No filename detected, defaulting to: " + fnm)
+    fil = open(fnm, mode='r')
+    msg = fil.read()
+    msg = msg.encode()
+    return msg
+
+
+def _writeFile(fnm: str, msg: bytes, vec: bytes):
+    if len(fnm) == 0:
+        fnm = "output.txt"
+        print("No filename detected, defaulting to: " + fnm)
+    fil = open(fnm, mode='w')
+    msg = b64encode(msg).decode()
+    vec = b64encode(vec).decode()
+    fil.write("-----BEGIN AES256-CBC MESSAGE-----\n\n")
+    fil.write(msg)
+    fil.write("\n\n-----END AES256-CBC MESSAGE-----\n\n")
+    fil.write("-----BEGIN AES256-CBC INITIALISATION VECTOR-----\n\n")
+    fil.write(vec)
+    fil.write("\n\n-----END AES256-CBC INITIALISATION VECTOR-----\n\n")
 
 
 def _encrypt(message):
@@ -61,9 +74,11 @@ passphrase = input("Please enter passphrase: ")
 key = _createKey(passphrase)
 # Input filename
 filename = input("Please enter a filename to be encrypted: ")
-message = _createMessage(filename)
+message = _readFile(filename)
 # Generate Initialisation Vector (IV)
 iv = os.urandom(16)
-print("The Initialisation Vector (IV) is:  " + b64encode(iv).decode())
+# print("The Initialisation Vector (IV) is:  " + b64encode(iv).decode())
 secret_message = _encrypt(message)
+outFile = input("Please enter a filename for the encryption output: ")
+_writeFile(outFile, secret_message, iv)
 deciphered = _decrypt(secret_message)
